6b754cf25064b9b1e130a08c64caf294e7e2f664
ivan <ivan.portillo@mo2o.com>
2024-04-18 09:40:11 +0200

    6-controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'app_conference')]
-    public function index(): Response
+    #[Route('/hello/{name}', name: 'homepage')]
+    public function index(string $name = ''): Response
     {
+        $greet = '';
+        if ($name) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(
             '<html>
-                <body>
-                    <img src="/images/under-construction.gif" />
+                <body>'.
+                $greet
+                    .'<img src="/images/under-construction.gif" />
                 </body>
             </html>'
 
